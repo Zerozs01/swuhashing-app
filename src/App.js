7bedf6433857c4ae7241f961a97511037ab0c84e
Dkzeros <auandmaxma@gmail.com>
2023-11-15 23:22:49 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+
 
 import Header from './components/Header';
 import About from './components/About';
@@ -7,12 +7,9 @@ import Footer from './components/Footer';
 import Informationhomepage from './components/Information homepage';
 
 function App() {
-  const navigate = useNavigate();
 
-  // Handle navigating back to the previous page
-  const handleGoBack = () => {
-    navigate(-1); // Go back one step in the history
-  };
+
+ 
 
   return (
     <>
@@ -20,7 +17,7 @@ function App() {
       <Informationhomepage />
       <About />
       <Footer />
-      <button onClick={handleGoBack}>Go Back</button>
+     
     </>
   );
 }
