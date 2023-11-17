3de17b197b446dd4125e98700749462af9deaa27
Dkzeros <auandmaxma@gmail.com>
2023-11-17 15:10:39 +0700

    server

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,9 +1,12 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import bcrypt from 'bcryptjs';
+import Axios from 'axios';
 
 const Signin = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+
+  const [usersystem, setusersystem] = useState([]);
 
   const isValidEmail = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -26,6 +29,15 @@ const Signin = () => {
     console.log(hashedPassword);
     window.localStorage.setItem('login', JSON.stringify({ email: hashedEmail, hashedPassword }));
     console.log("account create success");
+    Axios.post('http://localhost:3001/usersystem', {
+      email: hashedEmail,
+      password: hashedPassword
+    }).then(() => {
+      setusersystem([...usersystem, {
+        email: hashedEmail,
+        password: hashedPassword
+      }])
+    });
   };
 
   const LoginForm = (e) => {
@@ -44,7 +56,7 @@ const Signin = () => {
       return;
     }
 
-    const getHashedEmail = storedData.email; // Assuming you store hashed email directly during signup
+    const getHashedEmail = storedData.email;
     const getHashedPassword = storedData.hashedPassword;
 
     bcrypt.compare(email, getHashedEmail, function (error, isMatch) {
@@ -52,10 +64,8 @@ const Signin = () => {
         throw error;
       } else if (!isMatch) {
         console.log("Email doesn't match!");
-        // You can show an alert or update the UI for incorrect email
       } else {
         console.log("Email matches!");
-        // You can show an alert or update the UI for correct email
       }
     });
 
@@ -64,22 +74,23 @@ const Signin = () => {
         throw err;
       } else if (!isMatch) {
         console.log("Password doesn't match!");
-        // You can show an alert or update the UI for incorrect password
       } else {
         console.log("Password matches!");
-        // You can show an alert or update the UI for successful login
       }
     });
   };
+
+  const getusersystem = () => {
+    Axios.get('http://localhost:3001/usersystem').then((response) => { setusersystem(response.data); });
+  }
 
   return (
     <div>
       <section className="py-5">
         <div className="container px-5">
-          {/* Contact htmlForm*/}
           <div className="bg-light rounded-4 py-5 px-4 px-md-5 text-center">
-            <div className=" mb-3">
-              <div className="text-gradient fw-bold">Email</div><br></br>
+            <div className="mb-3">
+              <div className="text-gradient fw-bold">Email</div><br />
               <input
                 type="email"
                 placeholder="email"
@@ -87,8 +98,8 @@ const Signin = () => {
                 style={{ padding: '15px', borderRadius: '10px', margin: '10px' }}
               />
             </div>
-            <div className=" mb-3">
-              <div className="text-gradient fw-bold">Password</div><br></br>
+            <div className="mb-3">
+              <div className="text-gradient fw-bold">Password</div><br />
               <input
                 type="password"
                 placeholder="password"
@@ -113,6 +124,22 @@ const Signin = () => {
             >
               Login
             </button>
+            <div>
+              <button onClick={getusersystem} className="text-light bg-dark" style={{ padding: '25px', borderRadius: '10px', margin: '20px' }}>
+                Show user information
+              </button>
+              {usersystem.map((val, key) => {
+                return (
+                  <div className="card" key={key}>
+                    <div className="text-center card-body">
+                      <p className="card-text">id: {val.user_id}</p>
+                      <p className="card-text">Email: {val.email}</p>
+                      <p className="card-text">password: {val.password}</p>
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
           </div>
         </div>
       </section>
